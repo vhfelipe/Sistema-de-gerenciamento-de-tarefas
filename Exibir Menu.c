d1effbf60b5958a6a571b70fcaa3b71863fd9887
vhfelipe <vhfeliperamos@gmail.com>
2025-06-09 08:09:52 -0300

    Create Exibir Menu.c

---
+++ Exibir Menu.c
@@ -0,0 +1,10 @@
+void exibirMenu() {
+    printf("\n=== Sistema de Gerenciamento de Tarefas ===\n");
+    printf("1. Cadastrar Tarefa\n");
+    printf("2. Listar Tarefas\n");
+    printf("3. Editar Tarefa\n");
+    printf("4. Excluir Tarefa\n");
+    printf("5. Salvar Tarefas em Arquivo\n");
+    printf("6. Sair\n");
+    printf("Escolha uma opção: ");
+}
